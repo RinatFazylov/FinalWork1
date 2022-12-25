5cd5316115cbcb3a83357d9ab8608001cb9c09c8
Rinat <arahnid7master@gmail.com>
2022-12-25 19:47:47 +0300

    Метод печати массива

--- controlwork/Program.cs
+++ controlwork/Program.cs
@@ -1,3 +1,14 @@
 ﻿Console.WriteLine("Введите строки через пробел");
 string element = Console.ReadLine();
 string[] array = element.Split(' ');
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
